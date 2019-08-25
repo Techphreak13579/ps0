55c6a932a3cfabf9e9333ce8aedd05c3fb2a45eb
Techphreak13579 <52937531+Techphreak13579@users.noreply.github.com>
2019-08-25 16:14:36 +0100

    Add files via upload

---
+++ abraham.py
@@ -0,0 +1,16 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> x=int(input("Enter a number for x:"))
+Enter a number for x:2
+>>> y=int(input("Enter a number for y:"))
+Enter a number for y:3
+>>> power = x**y
+>>> print(x,"raised to the power of",y, "is" power)
+SyntaxError: invalid syntax
+>>> print(x,"raised to the power of",y, "is" ,power)
+2 raised to the power of 3 is 8
+>>> log=(x)=1
+>>> print("log base 2 of",x,"is",log)
+log base 2 of 1 is 1
+>>> 
